f261ae2665508ee32440c5dd2e26ed0b82a73595
waqasowais12 <139681510+waqasowais12@users.noreply.github.com>
2025-11-30 03:40:37 +0500

    Update streamlit_app.py
    Commit change because stremlit app during deploy crashed

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
+st.title('My Parents New Healthy Diner')
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
